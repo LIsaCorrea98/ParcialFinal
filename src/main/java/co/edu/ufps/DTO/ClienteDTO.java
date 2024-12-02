4bac91a8695c087297f5fb4db5b754929e28beef
Isabella Correa <pruebaslaura19@gmail.com>
2024-12-02 12:42:10 -0500

    me faltaba modificar un dto q pena

--- src/main/java/co/edu/ufps/DTO/ClienteDTO.java
+++ src/main/java/co/edu/ufps/DTO/ClienteDTO.java
@@ -1,5 +1,18 @@
 package co.edu.ufps.DTO;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class ClienteDTO {
 
+    private Integer id;
+    private String nombre;
+    private String direccion;
+    private String telefono;
+    private String email;
 }
+
